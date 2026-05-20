6ee129473fb76af95e4e5577a9d59738e84942b0
Logesh Ramu R <logeshram2244@gmail.com>
2026-05-21 00:39:34 +0530

    Fixed white login screen

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,20 +1,22 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate, Link } from "react-router-dom";
 
-function Login() {
-  const navigate = useNavigate();
+const Login = () => {
 
-  const API = "https://task-manager-app-kjwe.onrender.com";
+  const navigate = useNavigate();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const API = "https://task-manager-app-kjwe.onrender.com";
 
   const handleLogin = async (e) => {
     e.preventDefault();
 
     try {
-      const response = await axios.post(
+
+      const res = await axios.post(
         `${API}/api/auth/login`,
         {
           email,
@@ -22,27 +24,30 @@ function Login() {
         }
       );
 
-      console.log(response.data);
+      console.log(res.data);
 
-      localStorage.setItem("token", response.data.token);
+      localStorage.setItem("token", res.data.token);
 
       alert("Login Successful");
 
       navigate("/dashboard");
 
-    } catch (error) {
-      console.log(error);
+    } catch (err) {
 
-      if (error.response) {
-        alert(error.response.data.message);
+      console.log(err);
+
+      if (err.response) {
+        alert(err.response.data.message);
       } else {
         alert("Server Error");
       }
+
     }
   };
 
   return (
     <div className="h-screen flex items-center justify-center bg-gray-200">
+
       <div className="bg-white p-8 rounded-xl shadow-lg w-96">
 
         <h1 className="text-3xl font-bold text-center mb-6">
@@ -54,24 +59,24 @@ function Login() {
           <input
             type="email"
             placeholder="Email"
+            className="w-full border p-3 mb-4 rounded-lg"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            className="w-full border p-3 mb-4 rounded-lg"
             required
           />
 
           <input
             type="password"
             placeholder="Password"
+            className="w-full border p-3 mb-4 rounded-lg"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            className="w-full border p-3 mb-4 rounded-lg"
             required
           />
 
           <button
             type="submit"
-            className="w-full bg-blue-600 text-white p-3 rounded-lg hover:bg-blue-700"
+            className="w-full bg-blue-600 text-white p-3 rounded-lg"
           >
             Login
           </button>
@@ -86,8 +91,9 @@ function Login() {
         </p>
 
       </div>
+
     </div>
   );
-}
+};
 
 export default Login;
