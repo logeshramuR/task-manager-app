d2d2ec16e3c1552c28e5b8f3c5af99aa2257d3f6
Logesh Ramu R <logeshram2244@gmail.com>
2026-05-21 00:48:50 +0530

    Added dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
         <Route path="/register" element={<Register />} />
 
         <Route path="/login" element={<Login />} />
+
+        <Route path="/dashboard" element={<Dashboard />} />
 
       </Routes>
 
