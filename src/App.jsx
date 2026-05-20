e3bd74c5ae3c3294926a442ad14c72d7a7ff7792
Logesh Ramu R <logeshram2244@gmail.com>
2026-05-21 00:42:37 +0530

    Fixed app routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
+
       <Routes>
-        <Route path="/" element={<Login />} />
 
         <Route path="/register" element={<Register />} />
 
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+
       </Routes>
+
     </BrowserRouter>
   );
 }
