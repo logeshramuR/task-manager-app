b71e66f8fd5851ef1752ee0d257a3e3b31272b51
Logesh Ramu R <logeshram2244@gmail.com>
2026-05-21 14:17:51 +0530

    Added dashboard task manager

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,68 @@
+import { useState } from "react";
+
 function Dashboard() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = () => {
+    if (task.trim() === "") return;
+
+    setTasks([...tasks, task]);
+    setTask("");
+  };
+
+  const deleteTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-200">
+    <div className="min-h-screen bg-gray-200 flex justify-center items-center">
 
-      <div className="bg-white p-10 rounded-xl shadow-lg">
+      <div className="bg-white p-8 rounded-xl shadow-lg w-[500px]">
 
-        <h1 className="text-4xl font-bold text-green-600">
-          Welcome to Dashboard
+        <h1 className="text-3xl font-bold text-center text-green-600 mb-6">
+          Task Manager
         </h1>
+
+        <div className="flex gap-2 mb-6">
+
+          <input
+            type="text"
+            placeholder="Enter Task"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+            className="flex-1 border p-3 rounded-lg"
+          />
+
+          <button
+            onClick={addTask}
+            className="bg-green-600 text-white px-5 rounded-lg"
+          >
+            Add
+          </button>
+
+        </div>
+
+        <div>
+
+          {tasks.map((t, index) => (
+            <div
+              key={index}
+              className="flex justify-between items-center bg-gray-100 p-3 rounded-lg mb-3"
+            >
+              <p>{t}</p>
+
+              <button
+                onClick={() => deleteTask(index)}
+                className="bg-red-500 text-white px-3 py-1 rounded-lg"
+              >
+                Delete
+              </button>
+            </div>
+          ))}
+
+        </div>
 
       </div>
 
