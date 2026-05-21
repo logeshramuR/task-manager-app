b1fc47bcebf70e63fb81630370d06b6ca1099d8e
Logesh Ramu R <logeshram2244@gmail.com>
2026-05-21 20:26:31 +0530

    Added working cart feature

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 function Products() {
+  const [cart, setCart] = useState([]);
+
   const products = [
     {
       name: "iPhone 15",
@@ -20,11 +24,23 @@ function Products() {
     },
   ];
 
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+    alert(product.name + " added to cart");
+  };
+
   return (
     <div className="min-h-screen bg-gray-100 p-10">
-      <h1 className="text-4xl font-bold text-center mb-10 text-blue-600">
-        E-Commerce Products
-      </h1>
+
+      <div className="flex justify-between items-center mb-10">
+        <h1 className="text-4xl font-bold text-blue-600">
+          E-Commerce Products
+        </h1>
+
+        <div className="bg-blue-600 text-white px-5 py-2 rounded-lg">
+          Cart: {cart.length}
+        </div>
+      </div>
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
         {products.map((product, index) => (
@@ -46,7 +62,10 @@ function Products() {
               {product.price}
             </p>
 
-            <button className="mt-4 w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700">
+            <button
+              onClick={() => addToCart(product)}
+              className="mt-4 w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700"
+            >
               Add to Cart
             </button>
           </div>
